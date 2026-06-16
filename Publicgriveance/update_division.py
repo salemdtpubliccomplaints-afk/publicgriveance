842e010f34a76dde331b490fea9b2f288b8fdd75
விஜய் தமிழன் ஆ.பார்த்திபன் <salemdt.publiccomplaints@gmail.com>
2026-06-17 00:37:05 +0530

    Updated update_division file

--- Publicgriveance/update_division.py
+++ Publicgriveance/update_division.py
@@ -10,7 +10,22 @@ try:
     ADD COLUMN division_no TEXT
     """)
 
-    print("Division Number column added.")
+    print("Division No column added.")
+print("Zonal Office column added.")
+print("Assigned Officer column added.")
+print("Date of Complaint column added.")
+print("Ward Notification Status column added.")
+print("Ward Representative column added.")
+print("Response Details column added.")
+print("Informed To Dept column added.")
+print("Initial Action Status column added.")
+print("Progress Update Status column added.")
+print("Final Resolution Status column added.")
+print("Remarks & Notes column added.")  
+
+
+
+    
 
 except Exception as e:
     print(e)
