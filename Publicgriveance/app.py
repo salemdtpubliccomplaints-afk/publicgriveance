c097a46c5d443fce996e9726026a8bd6fb56e9e4
விஜய் தமிழன் ஆ.பார்த்திபன் <salemdt.publiccomplaints@gmail.com>
2026-06-17 12:18:20 +0530

    updated

--- Publicgriveance/app.py
+++ Publicgriveance/app.py
@@ -518,9 +518,4 @@ def logout():
 # ---------------------------
 
 if __name__ == "__main__":
-
-    app.run(
-        host="192.168.0.6",
-        port=5000,
-        debug=True
-    )
+    app.run(debug=True)
